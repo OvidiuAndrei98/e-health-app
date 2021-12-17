e884d89e2f0b014f688fb9fbe05b883804fd2995
Penica Andrei <penicaandrei@192-168-0-91.rdsnet.ro>
2021-12-17 11:10:07 +0200

    added backend

---
+++ backend/src/test/java/com/KatKool/ehealth/EHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KatKool.ehealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
